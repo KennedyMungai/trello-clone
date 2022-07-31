bd553f5800828f675ee044d347a08691410803c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 09:53:06 +0300

    Added the Card Container styling component

--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,4 @@
+import { cursorTo } from 'readline';
 import styled from 'styled-components';
 
 export const AppContainer = styled.div'
@@ -23,4 +24,14 @@ flex - grow: 0;
 export const ColumnTitle = styled.div'
 padding: 6px 16px 12px;
 font - weight: bold;
+'
+
+export const CardContainer = styled.div'
+background - color: #fff;
+cursor: pointer;
+margin - bottom: 0.5rem;
+padding: 0.5rem 1rem;
+max - width: 300px;
+border - radius: 3px;
+box - shadow: #091e4240 0px 1px 0px 0px;
 '
