2606c11c7cb5ac621afbcb3828463fa7821cb322
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-12 16:02:05 +0300

    Added some more html tags to the layout page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,7 +5,11 @@ type Props = {
 }
 
 const MarketingLayout = ({ children }: Props) => {
-	return <div>{children}</div>
+	return (
+		<div className='h-full bg-slate-100'>
+			<main className='pt-40 pb-20 bg-slate-100'>{children}</main>
+		</div>
+	)
 }
 
 export default MarketingLayout
