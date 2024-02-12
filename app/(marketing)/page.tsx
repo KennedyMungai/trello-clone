05012533a97949de6a56eacca960ecb2afcd3fc3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-12 16:14:11 +0300

    Added a call to action shenanigans thing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,14 +1,20 @@
-import { Medal } from "lucide-react"
+import { Medal } from 'lucide-react'
 
 const MarketingPage = () => {
 	return (
 		<div className='flex items-center justify-center flex-col'>
-			<div className="flex items-center justify-center flex-col">
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-          <Medal className="h-6 w-6 mr-2" />
-          No. 1 task management
-        </div>
-      </div>
+			<div className='flex items-center justify-center flex-col'>
+				<div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
+					<Medal className='h-6 w-6 mr-2' />
+					No. 1 task management
+				</div>
+				<h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>
+					Trello Clone Helps Teams Move
+				</h1>
+				<div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit'>
+					Work Forward.
+				</div>
+			</div>
 		</div>
 	)
 }
