cf01e04aed123b8b2fcfeee128e918bab4511e75
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-12 16:08:02 +0300

    Styled the innermost div in the page component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,8 +4,9 @@ const MarketingPage = () => {
 	return (
 		<div className='flex items-center justify-center flex-col'>
 			<div className="flex items-center justify-center flex-col">
-        <div className="">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
+          No. 1 task management
         </div>
       </div>
 		</div>
