880757ce5dab673f786296e19eaf372122308d3b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-12 16:06:20 +0300

    Added some CSS classes for the medal icon

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ const MarketingPage = () => {
 		<div className='flex items-center justify-center flex-col'>
 			<div className="flex items-center justify-center flex-col">
         <div className="">
-          <Medal />
+          <Medal className="h-6 w-6 mr-2" />
         </div>
       </div>
 		</div>
