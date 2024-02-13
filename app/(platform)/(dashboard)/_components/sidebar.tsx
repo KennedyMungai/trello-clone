897d753ab5ff5279a2000cbef60b2e4a0ca3c813
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 14:41:08 +0300

    Added a bunch of logic to the sidebar component

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,8 +1,54 @@
 'use client'
 
-type Props = {}
+import { Skeleton } from '@/components/ui/skeleton'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+import { useLocalStorage } from 'usehooks-ts'
 
-const SideBar = (props: Props) => {
+type Props = {
+	storageKey?: string
+}
+
+const SideBar = ({ storageKey = 't-sidebar-state' }: Props) => {
+	const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+		storageKey,
+		{}
+	)
+
+	const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+		useOrganization()
+
+	const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+		userMemberships: {
+			infinite: true
+		}
+	})
+
+	const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+		(acc: string[], key: string) => {
+			if (expanded[key]) {
+				acc.push(key)
+			}
+
+			return acc
+		},
+		[]
+	)
+
+	const onExpand = (id: string) => {
+		setExpanded((curr) => ({
+			...curr,
+			[id]: !expanded[id]
+		}))
+	}
+
+	if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+		return (
+			<>
+				<Skeleton />
+			</>
+		)
+	}
+
 	return <div>SideBar</div>
 }
 
