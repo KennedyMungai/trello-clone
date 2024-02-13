45e749b6a5dbb05f83200ffa262de0a06d02830f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 09:48:27 +0300

    Gone back to using inter as my system font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Roboto({
-	subsets: ['latin'],
-	weight: ['100', '300', '400', '500', '700', '900']
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
 	title: 'Trello Clone',
