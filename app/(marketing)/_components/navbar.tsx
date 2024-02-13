1f423bbb370911b00a30460ee3b49e15809f8bf3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 10:08:31 +0300

    Rendered out the Logo component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,3 +1,4 @@
+import Logo from '@/components/logo/logo'
 import React from 'react'
 
 type Props = {}
@@ -6,7 +7,7 @@ const NavBar = (props: Props) => {
 	return (
 		<div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
 			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-                Something
+                <Logo />
             </div>
 		</div>
 	)
