ebdf6572934ddf84ebe848f1aebdf65a6ea5ef87
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 12:00:45 +0300

    Rendered out the Navbar component

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react'
+import DashboardNavbar from './_components/navbar'
 
 type Props = {
 	children: ReactNode
 }
 
 const DashboardLayout = ({ children }: Props) => {
-	return <div className='h-full'>{children}</div>
+	return (
+		<div className='h-full'>
+			<DashboardNavbar />
+			{children}
+		</div>
+	)
 }
 
 export default DashboardLayout
