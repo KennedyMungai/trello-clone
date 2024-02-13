0cc2d644ab1e7a85090dda4e98e09adabcd397ef
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 11:12:01 +0300

    Created a layout file for the authentication pages

---
+++ app/(platform)/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+type Props = {
+	children: ReactNode
+}
+
+const AuthLayout = ({ children }: Props) => {
+	return <div className='h-full flex items-center justify-center'>{children}</div>
+}
+
+export default AuthLayout
