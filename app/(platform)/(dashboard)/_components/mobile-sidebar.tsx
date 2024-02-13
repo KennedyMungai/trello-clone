dd3dd3233a23e70c7904edf6fe8fc25546854bbc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 16:01:42 +0300

    Added the logic to prevent hydration errors

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,6 +1,24 @@
+'use client'
+
+import { useMobileSidebarStore } from '@/hooks/use-mobile-sidebar'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
 type Props = {}
 
 const MobileSidebar = (props: Props) => {
+	const pathname = usePathname()
+	const { isOpen, onClose, onOpen } = useMobileSidebarStore((state) => state)
+	const [isMounted, setIsMounted] = useState<boolean>(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+	if (!isMounted) {
+		return null
+	}
+
 	return <div>MobileSidebar</div>
 }
 
