05bd8e470da65b19913737d69271eb35670ca1ef
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 12:06:14 +0300

    Added some horizontal padding for the Nad=vBar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,15 +1,17 @@
-import Logo from "@/components/logo/logo"
+import Logo from '@/components/logo/logo'
 
 type Props = {}
 
 const DashboardNavbar = (props: Props) => {
-	return <div className="fixed top-0 z-50 w-full h-14 border-b shadow-sm bg-white flex items-center">
-        <div className="flex items-center gap-x-4">
-            <div className="hidden md:flex">
-                <Logo />
-            </div>
-        </div>
-    </div>
+	return (
+		<div className='fixed top-0 z-50 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+			<div className='flex items-center gap-x-4'>
+				<div className='hidden md:flex'>
+					<Logo />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default DashboardNavbar
