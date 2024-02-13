373ae7309b966faf5c6f3b26772d207ba0b0bb00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 14:05:30 +0300

    Added the logic to make sure the organization id in the website parameters matches the organization choses to be actove

--- app/(platform)/(dashboard)/organizations/[organizationId]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/organizations/[organizationId]/_components/org-control.tsx
@@ -1,8 +1,23 @@
 'use client'
 
+import { useOrganizationList } from '@clerk/nextjs'
+import { useParams } from 'next/navigation'
+import { useEffect } from 'react'
+
 type Props = {}
 
 const OrgControl = (props: Props) => {
+	const params = useParams()
+	const { setActive } = useOrganizationList()
+
+	useEffect(() => {
+		if (!setActive) return
+
+		setActive({
+			organization: params.organizationId as string
+		})
+	}, [setActive, params.organizationId])
+
 	return <div>OrgControl</div>
 }
 
