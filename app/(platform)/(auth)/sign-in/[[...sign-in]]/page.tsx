6e1f2fa9f98c9e8342c4653ce237ac1579cf6e9a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 11:09:43 +0300

    Retconned some changes

--- app/(platform)/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(platform)/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,11 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 type Props = {}
 
 const SignInPage = (props: Props) => {
-	return (
-		<div className='flex items-center justify-between'>
-			<SignIn />
-		</div>
-	)
+	return <SignIn />
 }
 
 export default SignInPage
--- app/(platform)/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(platform)/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,11 +4,7 @@ import React from 'react'
 type Props = {}
 
 const SignUpPage = (props: Props) => {
-	return (
-		<div className='flex items-center justify-between'>
-			<SignUp />
-		</div>
-	)
+	return <SignUp />
 }
 
 export default SignUpPage
