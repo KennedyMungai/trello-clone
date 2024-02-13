457ff192b0b726ae9c9423652c62e4835bf22389
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 12:19:13 +0300

    Added a userButton with an afterSignOutUrl and a bunch of props controlling their appearance

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,6 @@
 import Logo from '@/components/logo/logo'
 import { Button } from '@/components/ui/button'
-import { OrganizationSwitcher } from '@clerk/nextjs'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
 
 type Props = {}
@@ -34,6 +34,17 @@ const DashboardNavbar = (props: Props) => {
 								display: 'flex',
 								justifyContent: 'center',
 								alignItems: 'center'
+							}
+						}
+					}}
+				/>
+				<UserButton
+					afterSignOutUrl='/'
+					appearance={{
+						elements: {
+							avatarBox: {
+								height: 30,
+								width: 30
 							}
 						}
 					}}
