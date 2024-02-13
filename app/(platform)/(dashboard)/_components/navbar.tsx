2b382703f8744a0d300313c1048b9d8785c997d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 12:06:46 +0300

    Changed the top div to a nav

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,13 +4,13 @@ type Props = {}
 
 const DashboardNavbar = (props: Props) => {
 	return (
-		<div className='fixed top-0 z-50 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+		<nav className='fixed top-0 z-50 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center'>
 			<div className='flex items-center gap-x-4'>
 				<div className='hidden md:flex'>
 					<Logo />
 				</div>
 			</div>
-		</div>
+		</nav>
 	)
 }
 
