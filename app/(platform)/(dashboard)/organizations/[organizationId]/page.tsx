a14a203b5a3d0b8f34aee399350544a6b7f50bb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 11:53:40 +0300

    Added the hidePersonal prop

--- app/(platform)/(dashboard)/organizations/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organizations/[organizationId]/page.tsx
@@ -11,7 +11,7 @@ const OrganizationPage = ({ params: { organizationId } }: Props) => {
 
 	return (
 		<div className='h-full'>
-			<OrganizationSwitcher />
+			<OrganizationSwitcher hidePersonal />
 		</div>
 	)
 }
