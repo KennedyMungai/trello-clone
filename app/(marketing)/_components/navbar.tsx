76c9257a43d7d469de9e77711fc08ffaacf0686c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 10:13:09 +0300

    Added the login and Get Trello Clone for Free buttons

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,5 +1,5 @@
 import Logo from '@/components/logo/logo'
-import React from 'react'
+import { Button } from '@/components/ui/button'
 
 type Props = {}
 
@@ -8,6 +8,14 @@ const NavBar = (props: Props) => {
 		<div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
 			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
                 <Logo />
+                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+                    <Button>
+                        Login
+                    </Button>
+                    <Button>
+                        Get Trello Clone for Free
+                    </Button>
+                </div>
             </div>
 		</div>
 	)
