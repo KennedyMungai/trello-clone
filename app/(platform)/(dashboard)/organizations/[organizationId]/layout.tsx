f01ff787d8b63ecbfe4e4c857a7a2a45114bcf2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 14:00:31 +0300

    Added the OrgControl component to the individual organization layout file

--- app/(platform)/(dashboard)/organizations/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organizations/[organizationId]/layout.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react'
+import OrgControl from './_components/org-control'
 
 type Props = {
 	children: ReactNode
 }
 
 const IndividualOrganizationLayout = ({ children }: Props) => {
-	return <div className='h-full'>{children}</div>
+	return <>
+        <OrgControl />
+    {children}
+</>
 }
 
 export default IndividualOrganizationLayout
