c6b8ca3e7d31fe842f2cc89caadd9b9dc8ba059d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 15:39:22 +0300

    Showing all the buttons with their icons and labels in the accordion

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import {
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger
+} from '@/components/ui/accordion'
+import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
-import Image from 'next/image'
 import { Activity, CreditCard, Layout, Settings } from 'lucide-react'
+import Image from 'next/image'
+import { useRouter, usePathname } from 'next/navigation'
 
 export type Organization = {
 	id: string
@@ -20,6 +26,9 @@ type Props = {
 }
 
 const NavItem = ({ isActive, isExpanded, onExpand, organization }: Props) => {
+	const router = useRouter()
+	const pathname = usePathname()
+
 	const routes = [
 		{
 			label: 'Boards',
@@ -43,6 +52,10 @@ const NavItem = ({ isActive, isExpanded, onExpand, organization }: Props) => {
 		}
 	]
 
+	const onClick = (href: string) => {
+		router.push(href)
+	}
+
 	return (
 		<AccordionItem value={organization.id} className='border-none'>
 			<AccordionTrigger
@@ -65,6 +78,24 @@ const NavItem = ({ isActive, isExpanded, onExpand, organization }: Props) => {
 					</span>
 				</div>
 			</AccordionTrigger>
+			<AccordionContent className='pt-1 text-neutral-700'>
+				{routes.map((route, index) => (
+					<Button
+						key={index}
+						size={'sm'}
+						onClick={() => onClick(route.href)}
+						className={cn(
+							'w-full font-normal justify-start pl-10 mb-1',
+							pathname === route.href &&
+								'bg-sky-500/10 text-sky-700'
+						)}
+						variant={'ghost'}
+					>
+                        {route.icon}
+                        {route.label}
+                    </Button>
+				))}
+			</AccordionContent>
 		</AccordionItem>
 	)
 }
