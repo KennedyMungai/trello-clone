6e3d27a8f5d621d93d543cdbc6cb4cb79b731e0d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 10:22:48 +0300

    Gave the buttons some variant properties

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,22 +1,23 @@
 import Logo from '@/components/logo/logo'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
 		<div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
-			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-                <Logo />
-                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-                    <Button>
-                        Login
-                    </Button>
-                    <Button>
-                        Get Trello Clone for Free
-                    </Button>
-                </div>
-            </div>
+			<div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
+				<Logo />
+				<div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
+					<Button size={'sm'} variant={'outline'}>
+						<Link href='/sign-in'>Login</Link>
+					</Button>
+					<Button size={'sm'}>
+						Get Trello Clone for Free
+					</Button>
+				</div>
+			</div>
 		</div>
 	)
 }
