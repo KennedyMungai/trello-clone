497fb891e328a055ccd332941d559e774b0e963a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 12:04:50 +0300

    Added a logo to the Dashboard Navbar component

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,15 @@
+import Logo from "@/components/logo/logo"
 
 type Props = {}
 
 const DashboardNavbar = (props: Props) => {
-	return <div>DashboardNavbar</div>
+	return <div className="fixed top-0 z-50 w-full h-14 border-b shadow-sm bg-white flex items-center">
+        <div className="flex items-center gap-x-4">
+            <div className="hidden md:flex">
+                <Logo />
+            </div>
+        </div>
+    </div>
 }
 
 export default DashboardNavbar
