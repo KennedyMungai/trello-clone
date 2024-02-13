3d7226be9ee4b8005234816ecd6e054038dded08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 10:28:27 +0300

    Made some changes to the navbar and footer components

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -10,7 +10,7 @@ const NavBar = (props: Props) => {
 			<div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
 				<Logo />
 				<div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
-					<Button size={'sm'} variant={'outline'}>
+					<Button size={'sm'} variant={'outline'} asChild>
 						<Link href='/sign-in'>Login</Link>
 					</Button>
 					<Button size={'sm'} asChild>
