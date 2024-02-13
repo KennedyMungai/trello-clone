907b3443c2383849c3a613b04ec3430a6a820978
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 15:51:44 +0300

    Rendered the MobileSidebar component

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,12 +2,14 @@ import Logo from '@/components/logo/logo'
 import { Button } from '@/components/ui/button'
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
+import MobileSidebar from './mobile-sidebar'
 
 type Props = {}
 
 const DashboardNavbar = (props: Props) => {
 	return (
 		<nav className='fixed top-0 z-50 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+            <MobileSidebar />
 			<div className='flex items-center gap-x-4'>
 				<div className='hidden md:flex'>
 					<Logo />
