875566d101e186b57b7029a470cb5cbad4b1b070
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 12:55:39 +0300

    Formatted the component

--- app/(platform)/(dashboard)/organizations/layout.tsx
+++ app/(platform)/(dashboard)/organizations/layout.tsx
@@ -5,7 +5,11 @@ type Props = {
 }
 
 const OrganizationLayout = ({ children }: Props) => {
-	return <div className='h-full md:pt-4 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto'>{children}</div>
+	return (
+		<div className='h-full md:pt-4 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto'>
+			{children}
+		</div>
+	)
 }
 
 export default OrganizationLayout
