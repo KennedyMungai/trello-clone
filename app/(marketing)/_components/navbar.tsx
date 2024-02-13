0f7f6eae683817b66cde8a279627e7b681f90e42
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 10:07:38 +0300

    Added some classes to various divs for the component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <div>NavBar</div>
+	return (
+		<div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
+			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+                Something
+            </div>
+		</div>
+	)
 }
 
 export default NavBar
