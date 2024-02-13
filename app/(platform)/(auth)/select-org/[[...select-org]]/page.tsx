d12ba9775d25e87807cc30927cbdbaaa4c11ece3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-13 11:39:37 +0300

    Added some routing options after creating and after selecting an organization

--- app/(platform)/(auth)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(auth)/select-org/[[...select-org]]/page.tsx
@@ -3,7 +3,13 @@ import { OrganizationList } from '@clerk/nextjs'
 type Props = {}
 
 const CreateOrganizationPage = (props: Props) => {
-	return <OrganizationList hidePersonal />
+	return (
+		<OrganizationList
+			hidePersonal
+			afterSelectOrganizationUrl={'/organizations/:id'}
+			afterCreateOrganizationUrl={'/organizations/:id'}
+		/>
+	)
 }
 
 export default CreateOrganizationPage
