8d43fff51f1a71090c827a6ca25100ab538a8649
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-14 11:41:54 +0300

    Did some refactoring for the app

--- app/(platform)/(dashboard)/organizations/[organizationId]/form-input.tsx
+++ app/(platform)/(dashboard)/organizations/[organizationId]/form-input.tsx
@@ -1,7 +1,15 @@
 type Props = {}
 
 const FormInput = (props: Props) => {
-	return <div>Input</div>
+	return (
+		<input
+			id='title'
+			name='title'
+			required
+			placeholder='Enter a board title'
+			className='border border-black p-2 text-stone-500 font-semibold rounded-md'
+		/>
+	)
 }
 
 export default FormInput
--- app/(platform)/(dashboard)/organizations/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organizations/[organizationId]/form.tsx
@@ -3,6 +3,7 @@
 import { State, create } from '@/actions/createBoard'
 import { Button } from '@/components/ui/button'
 import { useFormState } from 'react-dom'
+import FormInput from './form-input'
 
 type Props = {}
 
@@ -13,13 +14,7 @@ const Form = (props: Props) => {
 	return (
 		<form action={dispatch}>
 			<div className='flex flex-col space-y-2'>
-				<input
-					id='title'
-					name='title'
-					required
-					placeholder='Enter a board title'
-					className='border border-black p-2 text-stone-500 font-semibold rounded-md'
-				/>
+				<FormInput />
 				{state?.errors?.title?.map((error, index) => (
 					<p key={index} className='text-rose-500'>
 						{error}
