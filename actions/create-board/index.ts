1d397eabe075aa71b2ab6ff8012a83f204c7fc0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-14 14:25:47 +0300

    Added some logic to the file

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -1,49 +1,36 @@
 'use server'
 
+import { auth } from '@clerk/nextjs'
+import { InputType, ReturnType } from './types'
 import db from '@/lib/db'
 import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
-import { z } from 'zod'
 
-export type State = {
-	errors?: {
-		title?: string[]
-	}
-	message?: string | null
-}
+const handler = async (data: InputType): Promise<ReturnType> => {
+	const { userId } = auth()
 
-const CreateBoard = z.object({
-	title: z
-		.string()
-		.min(3, { message: 'A minimum length of 3 letters is required' })
-})
-
-export async function create(prevState: State, formData: FormData) {
-	const validatedFields = CreateBoard.safeParse({
-		title: formData.get('title')
-	})
-
-	if (!validatedFields!.success) {
+	if (!userId) {
 		return {
-			errors: validatedFields.error.flatten().fieldErrors,
-			message: 'Missing Fields'
+			error: 'Unauthorized'
 		}
 	}
 
-	const title = validatedFields.data.title
+	const { title } = data
+
+	let board
 
 	try {
-		await db.board.create({
+		board = await db.board.create({
 			data: {
 				title
 			}
 		})
 	} catch (error) {
 		return {
-			message: 'Database Error'
+			error: 'Failed to create board'
 		}
 	}
 
-	revalidatePath('/organizations/org_2cIyNRFX3Cb2tPdSJP7hm4bPvoc')
-	redirect('/organizations/org_2cIyNRFX3Cb2tPdSJP7hm4bPvoc')
+	revalidatePath(`/board/${board.id}`)
+
+	return { data: board }
 }
