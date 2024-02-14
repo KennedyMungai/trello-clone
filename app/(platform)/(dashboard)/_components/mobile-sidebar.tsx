5b8accc76f3990da64b11cc499f6250a496086f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-14 08:33:18 +0300

    Added a sheet component

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,8 +1,21 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger
+} from '@/components/ui/sheet'
 import { useMobileSidebarStore } from '@/hooks/use-mobile-sidebar'
+import { Menu } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import SideBar from './sidebar'
 
 type Props = {}
 
@@ -23,7 +36,38 @@ const MobileSidebar = (props: Props) => {
 		return null
 	}
 
-	return <div>MobileSidebar</div>
+	return (
+		<>
+			<Button
+				onClick={onOpen}
+				className='block md:hidden'
+				variant={'ghost'}
+				size={'sm'}
+			>
+				<Menu className='h-4 w-4' />
+			</Button>
+			<Sheet open={isOpen} onOpenChange={onClose}>
+				<SheetTrigger asChild></SheetTrigger>
+				<SheetContent side={'left'} className='p-2 pt-10'>
+					{/* <SheetHeader>
+					<SheetTitle></SheetTitle>
+					<SheetDescription></SheetDescription>
+				</SheetHeader> */}
+					<SideBar storageKey='t-sidebar-mobile-state' />
+					<SheetFooter>
+						<SheetClose asChild>
+							<Button
+								type='submit'
+								className='fixed bottom-5 z-10'
+							>
+								Save Changes
+							</Button>
+						</SheetClose>
+					</SheetFooter>
+				</SheetContent>
+			</Sheet>
+		</>
+	)
 }
 
 export default MobileSidebar
