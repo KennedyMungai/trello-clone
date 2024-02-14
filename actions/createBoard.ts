19d59a6b02b04dcad02382f46db34ccec50bd479
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-14 10:22:22 +0300

    Deleted the console log from the server function

--- actions/createBoard.ts
+++ actions/createBoard.ts
@@ -5,8 +5,6 @@ import db from "@/lib/db"
 export async function create(formData: FormData) {
     const title = formData.get('title') as string
 
-    console.log('I got called')
-
     await db.board.create({
         data: {
             title
