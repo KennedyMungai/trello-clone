d67b13daf2142918b029fcda99b7f9f9f1ce181d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-14 11:29:19 +0300

    Added some markup to render the form errirs

--- app/(platform)/(dashboard)/organizations/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organizations/[organizationId]/form.tsx
@@ -7,19 +7,26 @@ import { useFormState } from 'react-dom'
 type Props = {}
 
 const Form = (props: Props) => {
-    const initialState: State = {message: null, errors: {}}
-    const [state, dispatch] = useFormState(create, initialState)
+	const initialState: State = { message: null, errors: {} }
+	const [state, dispatch] = useFormState(create, initialState)
 
 	return (
 		<form action={dispatch}>
-			<input
-				id='title'
-				name='title'
-				required
-				placeholder='Enter a board title'
-				className='border border-black p-2 text-stone-500 font-semibold rounded-md'
-			/>
-			<Button type='submit' className='ml-5'>
+			<div className='flex flex-col space-y-2'>
+				<input
+					id='title'
+					name='title'
+					required
+					placeholder='Enter a board title'
+					className='border border-black p-2 text-stone-500 font-semibold rounded-md'
+				/>
+				{state?.errors?.title?.map((error, index) => (
+					<p key={index} className='text-rose-500'>
+						{error}
+					</p>
+				))}
+			</div>
+			<Button type='submit' className='mt-3'>
 				Submit
 			</Button>
 		</form>
